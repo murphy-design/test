33a31588d7c7823c0a03d3caa16a00af1fd72c78
murphy-design <paul@murphy-design.co.uk>
2016-12-01 10:14:13 +0000

    Main Site
    Main Site

---
+++ wp-content/themes/twentysixteen-child/page.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Ajax
+*/
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<article class="article">
+			<!-- Ajax content will go here -->
+		</article>
+
+	</main><!-- .site-main -->
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
